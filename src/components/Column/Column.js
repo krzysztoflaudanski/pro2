210e5af1db13f2f0fff86759fa89a1dc1ae606d9
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-07-29 17:31:30 +0200

    add serch

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -5,14 +5,15 @@ import { useSelector } from 'react-redux';
 
 const Column = props => {
 
-    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id))
+    const searchString = useSelector(state => state.searchString);
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id && card.title.toLowerCase().includes(searchString.toLowerCase())))
 
     return (<article className={styles.column}>
         <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2>
         <ul className={styles.cards}>
             {cards.map(card => <Card key={card.id} title={card.title} />)}
         </ul>
-        <CardForm columnId={props.id}/>
+        <CardForm columnId={props.id} />
     </article>)
 
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,10 @@ const reducer = (state, action) => {
       return { ...state, columns: [...state.columns, { ...action.payload, id: shortid() }] };
     case 'ADD_CARD':
       return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }] };
-    default:
+case 'UPDATE_SEARCHSTRING':
+  return{ ...state, searchString: action.payload}
+        default:
+
       return state;
   }
 };
